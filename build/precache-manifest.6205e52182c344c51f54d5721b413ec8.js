2e4985493d305e4e6ca68829a0a85f80b34ba84d
Simon Kim <seungwoo.simon.kim@gmail.com>
2022-12-25 15:35:49 +0900

    deploy

---
+++ build/precache-manifest.6205e52182c344c51f54d5721b413ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0135fe23542aa7d9bc13176dc5af28",
+    "url": "/~ksimon12/index.html"
+  },
+  {
+    "revision": "6ad1157304e44c5f1742",
+    "url": "/~ksimon12/static/css/main.b933577c.chunk.css"
+  },
+  {
+    "revision": "87bd6ac3b3dbdbd60b7d",
+    "url": "/~ksimon12/static/js/2.1b42c95e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/~ksimon12/static/js/2.1b42c95e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad1157304e44c5f1742",
+    "url": "/~ksimon12/static/js/main.6a6f807f.chunk.js"
+  },
+  {
+    "revision": "6af4f8474b5ac8dc91b5",
+    "url": "/~ksimon12/static/js/runtime-main.d67bd0d0.js"
+  }
+]);
